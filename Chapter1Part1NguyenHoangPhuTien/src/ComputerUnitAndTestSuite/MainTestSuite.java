3f092c289572495176323fb0f2a5326a40869240
NguyenHoangPhuTien <tien.phu.nguyen01@gmail.com>
2016-08-22 22:20:22 +0700

    Update code

---
+++ Chapter1Part1NguyenHoangPhuTien/src/ComputerUnitAndTestSuite/MainTestSuite.java
@@ -0,0 +1,19 @@
+package ComputerUnitAndTestSuite;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import ManagerLibraryUnitAndTestSuite.TestSuiteForManagerLibrary;
+
+public class MainTestSuite {
+
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(TestSuite.class);
+		for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+			
+	      System.out.println(result.wasSuccessful());
+	}
+}
---
+++ Chapter1Part1NguyenHoangPhuTien/src/ComputerUnitAndTestSuite/TestSuite.java
@@ -0,0 +1,12 @@
+package ComputerUnitAndTestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ComputerTotalMoneyTest.class })
+public class TestSuite {
+
+	
+}
